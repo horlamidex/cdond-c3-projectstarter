4d200e489d872899d4b4814c4ff94294ea802ace
James Abraham <abraham.james@vendease.com>
2022-06-28 09:18:47 +0100

    Update persistent of data

--- .circleci/files/run.sh
+++ .circleci/files/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 describe-instances  --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:Project,Values=udapeople" --output text >> inventory.txt
+aws ec2 describe-instances  --query 'Reservations[*].Instances[*].PublicIpAddress' --filters "Name=tag:Project,Values=udapeople" --output text >> .circleci/ansible/inventory.txt
